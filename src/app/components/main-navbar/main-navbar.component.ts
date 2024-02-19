7c335e9b04164c995c39c02282b9c2add4cd9383
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-19 15:04:08 +0100

    routerLinkActive dependency added

--- src/app/components/main-navbar/main-navbar.component.ts
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-main-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './main-navbar.component.html',
   styleUrl: './main-navbar.component.scss'
 })
