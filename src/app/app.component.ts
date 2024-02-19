b2d4a0ab6d50d4bbb52fa3fb101152c9d7b6265e
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-19 13:57:54 +0100

    main navbar component routed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-main-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './main-navbar.component.html',
+  styleUrl: './main-navbar.component.scss'
+})
+export class MainNavbarComponent {
+
+}
