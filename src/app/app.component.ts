872246bbd7453d427e2618da73e85eface270fc3
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-20 10:54:34 +0100

    home component route defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainNavbarComponent],
+  imports: [CommonModule, RouterOutlet, MainNavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
