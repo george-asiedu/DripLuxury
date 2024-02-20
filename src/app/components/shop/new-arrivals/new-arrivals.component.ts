448933e4b6fb833ab586d1afe5b27b5c47dd85da
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-20 14:49:16 +0100

    loading and error implementations added

--- src/app/components/shop/new-arrivals/new-arrivals.component.ts
+++ src/app/components/shop/new-arrivals/new-arrivals.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../../service/products.service';
 import { Products } from '../../../model/products';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-new-arrivals',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './new-arrivals.component.html',
   styleUrl: './new-arrivals.component.scss'
 })
 export class NewArrivalsComponent implements OnInit {
   products: Products[] = []
+  loading: boolean = true
+  error: string = ''
+  faArrowRight = faArrowRight
 
   constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
     this.productsService.getProducts(4).subscribe(
       (data: any) => {
-        this.products = data;
+        this.products = data
+        this.loading  = false
       },
     ),
-    (error: any) => {
-      console.log('Error: ', error)
+    (error: string) => {
+      this.error = error
     }
   }
 }
