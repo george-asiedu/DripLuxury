c53dda99428d2efaa55193a6dcf57fc92826117a
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-20 13:40:02 +0100

    new arrivals component added to the home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ExploreComponent } from '../../components/shop/explore/explore.component';
+import { NewArrivalsComponent } from '../../components/shop/new-arrivals/new-arrivals.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    ExploreComponent
+    ExploreComponent,
+    NewArrivalsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
