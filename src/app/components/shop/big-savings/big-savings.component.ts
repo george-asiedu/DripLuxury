1e8b7a5e7ec983604b4ccd47e382245d111a529c
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-20 16:04:18 +0100

    down arrow imported

--- src/app/components/shop/big-savings/big-savings.component.ts
+++ src/app/components/shop/big-savings/big-savings.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-big-savings',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './big-savings.component.html',
   styleUrl: './big-savings.component.scss'
 })
 export class BigSavingsComponent {
-
+  faArrowDown = faArrowDown
 }
